c501eac1dbd726909025e019f4dd2cbc3c21838a
KriskoAdrienn <135051878+KriskoAdrienn@users.noreply.github.com>
2024-01-04 17:32:48 +0100

    Add files via upload

---
+++ feladat01.js
@@ -0,0 +1,8 @@
+
+let nev="Kriskó Adrienn Mercédesz";
+let csoport="#Team13";
+let HTML="HTML: 90%";
+let CSS="CSS: 90%";
+let JavaScript="JavaScript: 90%"; 
+
+document.write(nev+", "+csoport+", "+HTML+", "+CSS+", "+JavaScript);
