0ea7754ba15a29e46408cee0cbdd2da8f1737fba
KriskoAdrienn <135051878+KriskoAdrienn@users.noreply.github.com>
2024-02-06 18:09:13 +0100

    Delete feladat01.js

--- feladat01.js
+++
@@ -1,9 +0,0 @@
-<script>
-let nev="Kriskó Adrienn Mercédesz";
-let csoport="#Team13";
-let HTML="HTML: 90%";
-let CSS="CSS: 90%";
-let JavaScript="JavaScript: 90%"; 
-
-document.write(nev+", "+csoport+", "+HTML+", "+CSS+", "+JavaScript);
-</script>
